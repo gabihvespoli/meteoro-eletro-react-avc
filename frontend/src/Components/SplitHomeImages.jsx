796a461ae098fa326ea8fee986c8be535da73d4e
Gabriela Vespoli <gabiat3@hotmail.com>
2021-02-10 03:06:16 -0300

    initial commit

---
+++ frontend/src/Components/SplitHomeImages.jsx
@@ -0,0 +1,33 @@
+function HomeImages() {
+  return (
+    <div className="container d-flex justify-content-center align-items-center">
+      <div className="row">
+        <div className="col-sm foto">
+          <img
+            width="100%"
+            height="100%"
+            src="./img/img_bv.jpg"
+            alt="Imagem ilustrando compras na era digital"
+          />
+        </div>
+        <div className="col-sm foto">
+          <img
+            width="100%"
+            height="100%"
+            src="./img/img_bv2.jpg"
+            alt="Imagem ilustrando compras na era digital"
+          />
+        </div>
+        <div className="col-sm foto">
+          <img
+            width="100%"
+            height="100%"
+            src="./img/img_bv3.jpg"
+            alt="Imagem ilustrando compras na era digital"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default HomeImages;
---
+++ frontend/src/Components/SplitInfoContato.jsx
@@ -0,0 +1,26 @@
+function InfoContato() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm text-center">
+          <img
+            width="70"
+            src="img/Email.png"
+            alt="Imagem de uma cartinha com @"
+          />
+          <p className="lead mt-2">contato@meteoroeletro.com</p>
+        </div>
+        <div className="col-sm mb-5 text-center">
+          <img
+            width="70"
+            src="img/Whatsapp.png"
+            alt="Imagem do logo do Whatsapp"
+          />
+          <p className="lead mt-2">(11) 93333-3333</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InfoContato;
